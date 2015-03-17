fd440401e1d52a99c3cbed08375a8c92ccd3ac90
Steven Yue <jincheker@gmail.com>
2015-03-17 18:13:24 -0400

    Include hubberlyzer gem.

--- scripts/generator.rb
+++ scripts/generator.rb
@@ -1,4 +1,16 @@
+require 'hubberlyzer'
 require 'erb'
+
+# Fetch information
+hubberlyzer = Hubberlyzer::Profiler.new
+links = hubberlyzer.githubber_links("https://github.com/about/team")
+
+if links.length == 0
+	puts "Error! Failed to fetch any links."
+	return
+end
+
+info = hubberlyzer.fetch_profile_pages(links)
 
 root_path = File.expand_path("../../views", __FILE__)
                  
